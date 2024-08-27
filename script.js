3f87f30e563c4a08b9fe9790f6c167ba744eb026
Ujjwal Rai <157592531+ujjwalrai17@users.noreply.github.com>
2024-08-27 16:20:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+//we use "submit event" of form
+
+//this will give empty value.
+//  const height=parseInt(document.querySelector('#height').value)
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
